5356c0c719d33fbf69915137a038a34009d12904
d0m0s159 <dominik.tulach@gmail.com>
2024-09-25 10:37:21 +0200

    init-register

---
+++ frontend/quasar-project/src/pages/RegisterPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="column flex-center">
+    <div>
+      <q-input v-model="inputValue" :outlined="true" label="E-mail" />
+
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputValue: 'fdsjfklajd',
+    };
+  },
+};
+</script>
