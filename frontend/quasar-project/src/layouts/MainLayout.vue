4f3de198274918c12967ba389d7d7df209ae18f0
Java <tomcer11@gmail.com>
2024-09-28 20:05:41 +0200

    Initial header

--- frontend/quasar-project/src/layouts/MainLayout.vue
+++ frontend/quasar-project/src/layouts/MainLayout.vue
@@ -1,106 +1,56 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-secondary text-white">
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
         <q-toolbar-title>
-          Quasar App
+          Slack
         </q-toolbar-title>
+        <q-btn 
+          round 
+          color="white"
+          @click="menu = !menu"
+          ref="dropdownButton">
+          <q-avatar size="28px">
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo.svg">
+          </q-avatar>
+        </q-btn>
+        <q-menu
+          v-model="menu"
+          anchor="bottom right"
+          self="top right"
+          :target="$refs.menuButton">
+          <q-list style="min-width: 120px">
+            <q-item clickable v-close-popup>
+              <q-item-section>Profile</q-item-section>
+            </q-item>
+            <q-item clickable v-close-popup>
+              <q-item-section side>
+                <q-avatar
+                icon="directions"/>
+              </q-item-section>
+              <q-item-section>Log out</q-item-section>
+            </q-item>
+          </q-list>
 
-        <div>Quasar v{{ $q.version }}</div>
+        </q-menu>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
-      <router-view />
+      <router-view/>
     </q-page-container>
   </q-layout>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue';
-import EssentialLink, { EssentialLinkProps } from 'components/EssentialLink.vue';
+<style scoped>
+</style>
 
-defineOptions({
-  name: 'MainLayout'
-});
+<script>
+export default {
+  data () {
 
-const linksList: EssentialLinkProps[] = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
+    return {
+    }
   }
-];
-
-const leftDrawerOpen = ref(false);
-
-function toggleLeftDrawer () {
-  leftDrawerOpen.value = !leftDrawerOpen.value;
 }
 </script>
