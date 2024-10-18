e5f403bf2015f862e489a9b6f39222d8d0d98654
d0m0s159 <dominik.tulach@gmail.com>
2024-10-18 20:14:56 +0200

    made channels joinable

--- frontend/slack-clone/src/components/stores/useChannelsStore.ts
+++ frontend/slack-clone/src/components/stores/useChannelsStore.ts
@@ -5,123 +5,94 @@ export const useChannelsStore = defineStore('channels', {
     state: () => {
         return {
             channelList: [
-                { messageList: [
-                    {
-                        name: 'Peter',
-                        text: ['Ahoj'],
-                        stamp: 'dnes',
-                    },
-                    {
-                        name: 'Peter',
-                        text: ['Ahoj'],
-                        stamp: 'dnes',
-                    },
-                    {
-                        name: 'Peter',
-                        text: ['Ahoj'],
-                        stamp: 'dnes',
-                    },
-                    {
-                        name: 'Peter',
-                        text: ['Ahoj'],
-                        stamp: 'dnes',
-                    },
-                    {
-                        name: 'Peter',
-                        text: ['Ahoj'],
-                        stamp: 'dnes',
-                    },
-                    {
-                        name: 'Peter',
-                        text: ['Ahoj'],
-                        stamp: 'dnes',
-                    },
-                    {
-                        name: 'Peter',
-                        text: ['Ahoj'],
-                        stamp: 'dnes',
-                    },
-                    {
-                        name: 'Peter',
-                        text: ['Ahoj'],
-                        stamp: 'dnes',
-                    },
-                    {
-                        name: 'Peter',
-                        text: ['Ahoj'],
-                        stamp: 'dnes',
-                    },
-                    {
-                        name: 'Peter',
-                        text: ['Ahoj'],
-                        stamp: 'dnes',
-                    },
-                    {
-                        name: 'Peter',
-                        text: ['Ahoj'],
-                        stamp: 'dnes',
-                    },
-                    {
-                        name: 'Peter',
-                        text: ['Ahoj'],
-                        stamp: 'dnes',
-                    },
-                    {
-                        name: 'Peter',
-                        text: ['Ahoj'],
-                        stamp: 'dnes',
-                    },
-                    {
-                        name: 'Peter',
-                        text: ['Ahoj'],
-                        stamp: 'dnes',
-                    },
-                    {
-                        name: 'Peter',
-                        text: ['Ahoj'],
-                        stamp: 'dnes',
-                    },
-                    {
-                        name: 'Peter',
-                        text: ['Ahoj'],
-                        stamp: 'dnes',
-                    },
-                    {
-                        name: 'Peter',
-                        text: ['Ahoj'],
-                        stamp: 'dnes',
-                    },
-                    {
-                        name: 'Peter',
-                        text: ['Ahoj'],
-                        stamp: 'dnes',
-                    },
-                    {
-                        name: 'Peter',
-                        text: ['Ahoj'],
-                        stamp: 'dnes',
-                    },
-                    {
-                        name: 'Peter',
-                        text: ['Ahoj'],
-                        stamp: 'dnes',
-                    },
-                    {
-                        name: 'Peter',
-                        text: ['Ahoj'],
-                        stamp: 'dnes',
-                    },
-                ],
-                 name: 'General', 
-                 userlist: [], 
-                 admin: '', 
-                 public: true},
+                {
+                    messageList: [
+                        { name: 'Peter', text: ['Ahoj'], stamp: 'dnes' },
+                        { name: 'Peter', text: ['Ahoj'], stamp: 'dnes' },
+                        { name: 'Peter', text: ['Ahoj'], stamp: 'dnes' },
+                        { name: 'Peter', text: ['Ahoj'], stamp: 'dnes' },
+                        { name: 'Peter', text: ['Ahoj'], stamp: 'dnes' },
+                        { name: 'Peter', text: ['Ahoj'], stamp: 'dnes' },
+                        { name: 'Peter', text: ['Ahoj'], stamp: 'dnes' },
+                        { name: 'Peter', text: ['Ahoj'], stamp: 'dnes' },
+                        { name: 'Peter', text: ['Ahoj'], stamp: 'dnes' },
+                        { name: 'Peter', text: ['Ahoj'], stamp: 'dnes' },
+                        { name: 'Peter', text: ['Ahoj'], stamp: 'dnes' },
+                        { name: 'Peter', text: ['Ahoj'], stamp: 'dnes' },
+                        { name: 'Peter', text: ['Ahoj'], stamp: 'dnes' },
+                        { name: 'Peter', text: ['Ahoj'], stamp: 'dnes' },
+                        { name: 'Peter', text: ['Ahoj'], stamp: 'dnes' },
+                        { name: 'Peter', text: ['Ahoj'], stamp: 'dnes' },
+                        { name: 'Peter', text: ['Ahoj'], stamp: 'dnes' },
+                        { name: 'Peter', text: ['Ahoj'], stamp: 'dnes' },
+                        { name: 'Peter', text: ['Ahoj'], stamp: 'dnes' },
+                        { name: 'Peter', text: ['Ahoj'], stamp: 'dnes' },
+                        { name: 'Peter', text: ['Ahoj'], stamp: 'dnes' },
+                    ],
+                    name: 'General',
+                    userlist: [],
+                    admin: '',
+                    public: true
+                },
+                {
+                    messageList: [
+                        { name: 'John', text: ['Hello'], stamp: 'today' },
+                        { name: 'John', text: ['Hello'], stamp: 'today' },
+                    ],
+                    name: 'Random',
+                    userlist: [],
+                    admin: '',
+                    public: true
+                },
+                {
+                    messageList: [
+                        { name: 'Alice', text: ['Hi'], stamp: 'today' },
+                        { name: 'Alice', text: ['Hi'], stamp: 'today' },
+                    ],
+                    name: 'Tech',
+                    userlist: [],
+                    admin: '',
+                    public: true
+                },
             ] as Channel[],
+            joinableChannelList: [
+              {
+                  messageList: [
+                      { name: 'John', text: ['Hello'], stamp: 'today' },
+                  ],
+                  name: 'Random',
+                  userlist: [],
+                  admin: '',
+                  public: true
+              },
+              {
+                  messageList: [
+                      { name: 'Alice', text: ['Hi'], stamp: 'today' },
+                  ],
+                  name: 'Tech',
+                  userlist: [],
+                  admin: '',
+                  public: true
+              },
+          ] as Channel[],
         }
     },
     actions: {
         sendMessage(mName: string, mText: string[], mStamp: string, index: number) {
-            this.channelList[index].messageList.push({name: mName, text: mText,stamp: mStamp});
+            this.channelList[index].messageList.push({name: mName, text: mText, stamp: mStamp});
+        },
+        createChannel(channelName: string) {
+          this.channelList.push({
+              messageList: [],
+              name: channelName,
+              userlist: [],
+              admin: '',
+              public: true
+          });
+        },
+        moveChannelToJoined(index: number) {
+          const channel = this.joinableChannelList.splice(index, 1)[0];
+          this.channelList.push(channel);
         },
     }
-})
+});
--- frontend/slack-clone/src/pages/IndexPage.vue
+++ frontend/slack-clone/src/pages/IndexPage.vue
@@ -4,24 +4,24 @@
       <div class="row justify-between">
         <div class="col-auto">
           <div class="channel-list">
-          <div v-for="n in 3" :key="n">
-            <q-btn no-caps square unelevated size="0px" padding="0px" color="teal">
-              <q-avatar class="channel-icon" text-color="black">
-                new
-              </q-avatar>
-            </q-btn>
-          </div>
-          <div v-for="(channel, index) in channels" :key="index">
-            <q-btn no-caps square unelevated size="0px" padding="0px" color="red">
-              <q-avatar class="channel-icon" text-color="black">
-                oh no
-              </q-avatar>
-            </q-btn>
-          </div>
+            <div v-for="(channel, index) in joinableChannels" :key="index">
+              <q-btn no-caps square unelevated size="0px" padding="0px" color="teal" @click="openJoinDialog(channel.name, index)">
+                <q-avatar class="channel-icon" text-color="black">
+                  {{ getInitials(channel.name) }}
+                </q-avatar>
+              </q-btn>
+            </div>
+            <div v-for="(channel, index) in channels" :key="index">
+              <q-btn no-caps square unelevated size="0px" padding="0px" color="red">
+                <q-avatar class="channel-icon" text-color="black">
+                  {{ getInitials(channel.name) }}
+                </q-avatar>
+              </q-btn>
+            </div>
           </div>
         </div>
         <div class="col q-ml-sm self-end q-mb-sm">
-          <div class="q-pa-md q-mt-xl row">
+          <div class="q-pa-md q-mt-sm row">
             <div style="width: 100%; max-width: 1000px">
               <q-scroll-area ref="scrollArea" style="height: 72vh;">
                 <q-infinite-scroll @load="onLoad" reverse :disable="infiniteScroll">
@@ -31,11 +31,12 @@
                     </div>
                   </template>
                   <MessageComponent
-                  v-for="(message, index) in messages"
-                  :key="index"
-                  :name="message.name"
-                  :text="message.text"
-                  :stamp="message.stamp"/>
+                    v-for="(message, index) in messages"
+                    :key="index"
+                    :name="message.name"
+                    :text="message.text"
+                    :stamp="message.stamp"
+                  />
                 </q-infinite-scroll>
               </q-scroll-area>
             </div>
@@ -50,6 +51,23 @@
         </div>
       </div>
     </div>
+
+    <q-dialog v-model="joinDialog" persistent>
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Join Channel</div>
+        </q-card-section>
+
+        <q-card-section>
+          Do you want to join the channel "{{ selectedChannel }}"?
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="Cancel" color="negative" v-close-popup />
+          <q-btn flat label="Join" color="positive" @click="joinChannel" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 
@@ -73,12 +91,11 @@
 </style>
 
 <script lang="ts">
-import { ref, computed } from 'vue';
-import MessageComponent from 'src/components/MessageComponent.vue'; 
+import { ref, computed, nextTick } from 'vue';
+import MessageComponent from 'src/components/MessageComponent.vue';
 import { useChannelsStore } from 'src/components/stores/useChannelsStore';
 import { Message } from 'src/components/message';
 import { QScrollArea } from 'quasar';
-import { nextTick } from 'vue';
 
 export default {
   components: {
@@ -88,10 +105,11 @@ export default {
     const store = useChannelsStore();
 
     const channels = computed(() => store.channelList);
+    const joinableChannels = computed(() => store.joinableChannelList);
 
     const messages = ref<Message[]>([]);
     const fullMessages = ref<Message[]>([]);
-    
+
     const index = ref(0);
     let limit = 7;
 
@@ -102,8 +120,11 @@ export default {
 
     const text = ref('');
     const scrollArea = ref<InstanceType<typeof QScrollArea> | null>(null);
-    const infiniteScroll = ref(false)
+    const infiniteScroll = ref(false);
     const loading = ref(true);
+    const joinDialog = ref(false);
+    const selectedChannel = ref('');
+    const selectedChannelIndex = ref<number | null>(null);
 
     const sendMsg = async () => {
       const date = new Date();
@@ -119,31 +140,48 @@ export default {
     };
 
     const scrollToEnd = () => {
-      if(scrollArea.value){
+      if (scrollArea.value) {
         scrollArea.value.setScrollPosition('vertical', 10000);
       }
-    }
+    };
 
-    const onLoad = (index:number, done: () => void) => {
+    const onLoad = (index: number, done: () => void) => {
       setTimeout(() => {
-          if(limit < fullMessages.value.length - limit){
-            limit += limit
-            messages.value = fullMessages.value.slice(-limit);
-          }
-          else if(limit < fullMessages.value.length){
-            limit = fullMessages.value.length
-            messages.value = fullMessages.value.slice(-limit);
-          }
-          else{
-            infiniteScroll.value = false;
-            loading.value = false;
-          }
-          done()
-        }, 2000)
-    }
+        if (limit < fullMessages.value.length - limit) {
+          limit += limit;
+          messages.value = fullMessages.value.slice(-limit);
+        } else if (limit < fullMessages.value.length) {
+          limit = fullMessages.value.length;
+          messages.value = fullMessages.value.slice(-limit);
+        } else {
+          infiniteScroll.value = false;
+          loading.value = false;
+        }
+        done();
+      }, 2000);
+    };
+
+    const getInitials = (name: string) => {
+      return name.split(' ').map(word => word[0]).join('').toLowerCase();
+    };
+
+    const openJoinDialog = (channelName: string, channelIndex: number) => {
+      selectedChannel.value = channelName;
+      selectedChannelIndex.value = channelIndex;
+      joinDialog.value = true;
+    };
+
+    const joinChannel = () => {
+      if (selectedChannelIndex.value !== null) {
+        store.moveChannelToJoined(selectedChannelIndex.value);
+        console.log(`Joined channel: ${selectedChannel.value}`);
+        joinDialog.value = false;
+      }
+    };
 
     return {
       channels,
+      joinableChannels,
       messages,
       text,
       scrollArea,
@@ -152,6 +190,11 @@ export default {
       sendMsg,
       scrollToEnd,
       onLoad,
+      getInitials,
+      openJoinDialog,
+      joinChannel,
+      joinDialog,
+      selectedChannel
     };
   }
 };
