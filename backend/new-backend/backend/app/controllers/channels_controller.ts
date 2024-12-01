eeee606624ccb483d071a1f206bcc5d558127de3
Java <tomcer11@gmail.com>
2024-12-01 15:13:40 +0100

    backend join, leave, listUsers; still som todos

--- backend/new-backend/backend/app/controllers/channels_controller.ts
+++ backend/new-backend/backend/app/controllers/channels_controller.ts
@@ -5,38 +5,97 @@ import { DateTime } from 'luxon'
 import { channelManager } from '#start/ws'
 
 export default class ChannelsController {
-    // Function to load channels for a specific user
+
     public async loadChannels({ request }: HttpContext) {
-      // Access the userId from the params object
       const userId = request.body().id
-  
-      // Fetch the user with their associated channels
       const user = await User.query()
         .where('id', userId)
-        .preload('channels')  // Make sure 'channels' is a valid relationship in the User model
+        .preload('channels')
         .first()
   
       if (!user) {
-        return { message: 'User not found' }
+        return { succes: false, message: 'User not found' }
       }
-  
-      // Return the channels associated with the user
-      return user.channels
+
+      return { succes: true, channels: user.channels}
     }
 
-    public async ensureChannel({ request }: HttpContext) {
-      const channelName = request.body().channelName;
-      const userId = request.body().user; // Assuming this is the ID of the user
+    public async joinChannel({ request }: HttpContext) {
+      const channelName = request.body().channelName
+      const userId = request.body().user
       const channel = await Channel.findBy('name', channelName)
+
       if(!channel){
-        await Channel.create({
+        const newChannel = await Channel.create({
           name: channelName,
           adminId: (userId != 0) ? userId : null,
           isPublic: true,
           lastActivity: DateTime.now(),
         })
         channelManager?.ensureNamespace(channelName)
+
+        const user = await User.find('id', userId)
+        await user?.related('channels').attach([newChannel.id])
       }
-      return
+      else{
+        if(channel.isPublic){
+          const user = await User.find('id', userId)
+          await user?.related('channels').attach([channel.id])
+        }
+        else{
+          // TODO: need to make invitation mechanism
+          const invitation = true
+          if(invitation){
+            const user = await User.find('id', userId)
+            await user?.related('channels').attach([channel.id])
+
+            return { success: true, message: 'Channel has been joined' }
+          }
+          else{
+            return { success: false, message: 'Channel is private. Invitation required' }
+          }
+        }
+      }
+      return { success: true, message: 'Channel has been created' }
+    }
+
+    public async leaveChannel({ request }: HttpContext){
+      const channelName = request.body().channelName
+      const userId = request.body().user
+      const user = await User.find('id', userId)
+      const channel = await Channel.findBy('name', channelName)
+
+      if(channel){
+        if( channel.admin === user){
+          // TODO: need to handle sockets for users that are joined to this channel
+          await channel.delete()
+          return { success: true, message: 'Channel has been deleted' }
+        }
+        await user?.related('channels').detach([channel.id])
+      }
+      else{
+        return { success: false, message: 'Channel does not exist' }
+      }
+      return { success: true, message: 'Channel has been left' }
+    }
+
+    public async listUsers( {request}: HttpContext ){
+      const channelName = request.body().channelName
+      const userId = request.body().user
+      const user = await User.find('id', userId)
+      const channel = await Channel.findBy('name', channelName)
+
+      if(channel){
+        const isRelated = await user?.related('channels').query().where('id', channel.id)
+
+        if (isRelated) {
+          const users = await channel.related('users').query()
+          return { succes: true, users: users}
+        } else {
+          return { success: false, message: 'User is not related to the channel' }
+        }
+      }
+
+      return { success: false, message: 'User or channel not found' }
     }
   }
